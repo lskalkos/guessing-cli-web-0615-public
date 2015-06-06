2daf3a64fab6f519ec0c187ab54bae117e6b8391
Lia Skalkos <hello@liaskalkos.net>
2015-06-06 10:38:38 -0400

    Built CLI guessing game.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,30 @@
 # Code your solution here!
+
+def run_guessing_game
+
+  puts "Guess a number between 1 and 6."
+
+  command = gets.strip #human guesses. store their guess/command. note that their entry gets turned into a string!
+
+
+  while command != "exit" do #allow user to exit when their input is exit
+
+    random_numer = rand(1..6).to_s
+
+    if command == random_numer #compare their guess to a random number, make sure to turn it into a string!
+      puts "You guessed the correct number!!"
+    else
+      puts "The computer guessed #{random_numer}."
+    end
+
+    puts "Guess a number between 1 and 6."
+
+    command = gets.strip
+  end
+
+  puts "Goodbye!"
+
+
+end
+
+#note to self -- seems like for computers to keep running a program they must enter always enter loop....
